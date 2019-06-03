806b238d7960606e5ea9ed2b3c508a7c7b6fab79
Moses <keyboardstaff@gmail.com>
2019-06-03 21:52:44 +0800

    Update translations

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-custom-locales
+# about: An plugin for adding Chinese locales.
+# version: 1.0
+
+register_locale("zh_CN", name: "Chinese (Chinese)", nativeName: "Chinese (中文)", fallbackLocale: "zh")
